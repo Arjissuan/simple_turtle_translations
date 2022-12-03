188b409f3d493f359540d06544203beaeb74e407
Arjissuan <nswa87@gmail.com>
2022-12-03 09:59:08 +0100

    pomoc() works properly now minor changes

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import turtle
-from random import *
+import random
 
 def powitanie():
     hello = "Witaj w naszym tłumaczeniu pakietu turtle.\n" \
@@ -10,9 +10,10 @@ def powitanie():
             "Każdy błąd nauczy Cię czegoś nowego.\n"
     return hello
 def pomoc():
-    with open("instrukcja.txt", "r") as file:
-        hint = file
-    return hint
+    with open("instrukcja.txt", "r", encoding='utf-8') as file:
+        for item in file.read().splitlines():
+            print(item)
+
 
 def czyszczenie_obrazu():
     return turtle.reset()
@@ -21,24 +22,27 @@ def usun():
     return turtle.clear()
 
 class Imie_mojego_zolwia:
-    kolory_linii = ["czerwony", "zielony", "czarny","losowy"]
-    shapes = ["turtle"]
+    kolory_linii = ["czerwony", "zielony", "czarny","żółty","losowy"]
+    shapes = ["turtle", ]
+    tru_clours = ["red", "green", "black", "yellow", ]
     def __init__(self):
         self.zolw = turtle.Turtle()
 
     def personalizacja(self, shape, kolor):
         try:
             self.zolw.shape(str(shape))
-            tru_clours = ["red", "green", "black", "yellow", ]
-            if kolor in ["czerwony", "zielony", "czarny", "żółty",]:
-                self.zolw.color(tru_clours[self.kolory_linii.index(kolor)])
+
+            if kolor in self.kolory_linii:
+                self.zolw.color(self.tru_clours[self.kolory_linii.index(kolor)])
             elif kolor == "losowy":
-                self.zolw.color(random.choice(("green", "yellow", "red", "black")))
+                self.zolw.color(random.choice(self.kolory_linii))
             else:
-                print("Zły kolor, dostępne to {}".format(self.kolory_linii))
+                return ("Ta funkcja przyjmuje wartości w postaci danych string.\n"
+                        " Na przykład:Marek.personalizacja(\"turtle\", \"czerwony\")"
+                        f"dostepne kolory i kształy:{self.kolory_linii}, {self.shapes}")
         except:
-            print("Ta funkcja przyjmuje wartości w postaci danych string.\n"
-                  " Na przykład:Marek.personalizacja(\"turtle\", \"czerwony\")"
+            return ("Ta funkcja przyjmuje wartości w postaci danych string.\n"
+                  " Na przykład:Marek.personalizacja(\"turtle\", \"czerwony\")\n"
                   f"dostepne kolory i kształy:{self.kolory_linii}, {self.shapes}")
 
     def w_lewo(self, ile):
@@ -56,6 +60,10 @@ class Imie_mojego_zolwia:
             self.zolw.forward(30)
 
     def wielokat(self, L_or_P, angle, speed, many):
+        if type(angle) != int or type(speed) != int or type(many) != int:
+            return "Niepoprawnie uzyta funkcja, przyjmuje ona 4 argumenty, pierwszym określasz czy żółw ma iść w prawo czy lewo\n" \
+                       "kolejne określają kąt i jakie dlugie odcinki ma przebyc żółw.\n" \
+                       "Na_przykład.wielokat(lewo, 2,3,99)"
         for item in range(many):
             if L_or_P== "prawo" or L_or_P == "w_prawo":
                 self.zolw.right(angle)
@@ -68,16 +76,18 @@ class Imie_mojego_zolwia:
                        "kolejne określają kąt i jakie dlugie odcinki ma przebyc żółw." \
                        "Na_przykład.wielokat(lewo, 2,3,99)"
 
-
-
-    def pen(self, rysowanie=True, kolor=None):
-        try:
-            if rysowanie in ["tak", "rysuj", "maluj", True, "True", "true"]:
-                self.zolw.pendown()
-            elif rysowanie in ["nie", "przestan", "stop", "niemaluj", False, "false", 'False']:
-                self.zolw.penup()
-
-            if kolor==None:
-                pass
-        except:
-            print("Ta funkcja jest aby włączyć lub wyłączyć rysowanie, uzyj jej Imie.pen() gdzie argumenty to True aby rysowac i false aby przestac.")
+    def pen(self, rysowanie, kolor):
+        if rysowanie in ["tak", "rysuj", "maluj", True, "True", "true"]:
+            self.zolw.pendown()
+        elif rysowanie in ["nie", "przestan", "stop", "niemaluj", False, "false", 'False']:
+            self.zolw.penup()
+        else:
+            return "Ta funkcja jest aby włączyć lub wyłączyć rysowanie, uzyj jej Imie.pen() gdzie argumenty to True aby rysowac i false aby przestac\n." \
+                   f"Kolory zaś to {self.kolory_linii}"
+        if kolor == None:
+            pass
+        elif kolor in self.kolory_linii:
+            self.zolw.pencolor(self.tru_clours[self.kolory_linii.index(kolor)])
+        else:
+            return "Ta funkcja jest aby włączyć lub wyłączyć rysowanie, uzyj jej Imie.pen() gdzie argumenty to True aby rysowac i false aby przestac\n." \
+                   f"Kolory zaś to {self.kolory_linii}"
